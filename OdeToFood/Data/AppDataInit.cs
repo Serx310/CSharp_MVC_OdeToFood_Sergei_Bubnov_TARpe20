054889057438346671ed2c2416453e498d7d42ae
Serx310 <sergeibubnov120@gmail.com>
2021-11-25 15:53:20 +0200

    Add seeding

---
+++ OdeToFood/Data/AppDataInit.cs
@@ -0,0 +1,31 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+    public class AppDataInit
+    {
+        public static void SeedRestaurant(ApplicationDbContext context)
+        {
+            if (!context.Restaurants.Any())
+            {
+                context.Restaurants.Add(new Restaurant
+                {
+                    Name = "Hesburger",
+                    City = "Turku",
+                    Country = "Finland"
+                });
+
+                context.Restaurants.Add(new Restaurant
+                            {
+                                Name = "Suliko",
+                                City = "Tallinn",
+                                Country = "Estonia"
+                            });
+        }
+        }
+    }
+}
